9cdf231824bdc59a25b121f1440298723f5ed540
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-06-25 01:13:57 +0700

    Add production build

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
 import './App.css';
 
+
 function App() {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  const fileName = params?.filename || 'Paper_Wallet__QRCode'
   const [videoSrc, setVideoSrc] = useState('');
-  const [message, setMessage] = useState('Click Start to transcode');
   const ffmpeg = createFFmpeg({
-    log: true,
+    log: process.env.NODE_ENV === 'development',
+    corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js'
   });
+
+  useEffect(() => {
+    if (!videoSrc) {
+      doTranscode()
+    }
+  })
+
   const doTranscode = async () => {
-    setMessage('Loading ffmpeg-core.js');
-    await ffmpeg.load();
-    setMessage('Start transcoding');
-    ffmpeg.FS('writeFile', 'test.avi', await fetchFile('/flame.avi'));
-    await ffmpeg.run('-i', 'test.avi', 'test.mp4');
-    setMessage('Complete transcoding');
-    const data = ffmpeg.FS('readFile', 'test.mp4');
-    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
-  };
+    let images = []
+    if (window.frameElement) {
+      images = window.frameElement.attributes["data-images"].value
+      images = JSON.parse(images)
+    }
+    if (images.length) {
+      const frameSpeed = images.length * 32;
+      await ffmpeg.load();
+      for (let i = 0; i < images.length; i += 1) {
+        ffmpeg.FS('writeFile', `img00${i}.png`, await fetchFile(images[i]));
+      }
+  
+      await ffmpeg.run('-framerate', '60', '-pattern_type', 'glob', '-i', '*.png', '-vf', `setpts=${frameSpeed}*PTS`, '-c:a', 'copy', '-shortest', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', `${fileName}.mp4`);
+  
+      const data = ffmpeg.FS('readFile', `${fileName}.mp4`);
+      for (let i = 0; i < images.length; i += 1) {
+        ffmpeg.FS('unlink', `img00${i}.png`);
+      }
+      
+      const videoUrl = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+      setVideoSrc(videoUrl);
+    }
+  }
+
   return (
     <div className="App">
-      <p/>
-      <video src={videoSrc} controls></video><br/>
-      <button onClick={doTranscode}>Start</button>
-      <p>{message}</p>
+      <div id="videourl">
+      {videoSrc || 'inprocess'}
+      </div>
+      <div id="videotag">
+        <video width="320" height="240" src={videoSrc} controls />
+      </div>
     </div>
   );
 }
