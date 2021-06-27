e5cc75b066177563e3d19bdd25cd58b663ffbfae
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-06-27 11:10:52 +0700

    Update video time Add new build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import './App.css';
 function App() {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const params = Object.fromEntries(urlSearchParams.entries());
-  const fileName = params?.filename || 'Paper_Wallet__QRCode'
   const [videoSrc, setVideoSrc] = useState('');
   const ffmpeg = createFFmpeg({
     log: true,
@@ -17,17 +16,21 @@ function App() {
   let currentSource = null
   window.addEventListener('message', (evt) => {
     currentSource = evt.source
-    doTranscode(evt.data)
+    if (!videoSrc) {
+      doTranscode(evt.data)
+    }
   }, false);
 
   const doTranscode = async (images) => {
     if (images.length) {
-      const frameSpeed = images.length * 32;
+      const frameSpeed = 30 * images.length;
+      const fileName = params?.filename || 'Paper_Wallet__QRCode'
       await ffmpeg.load();
       for (let i = 0; i < images.length; i += 1) {
         ffmpeg.FS('writeFile', `img00${i}.png`, await fetchFile(images[i]));
       }
-      await ffmpeg.run('-framerate', '60', '-pattern_type', 'glob', '-i', '*.png', '-vf', `setpts=${frameSpeed}*PTS`, '-c:a', 'copy', '-shortest', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', `${fileName}.mp4`);
+      await ffmpeg.run('-framerate', '24', '-pattern_type', 'glob', '-i', '*.png', '-vf', `setpts=${frameSpeed}.0*PTS`, '-c:v', 'libx264', '-pix_fmt', 'yuv420p', `${fileName}.mp4`);
+
       const data = ffmpeg.FS('readFile', `${fileName}.mp4`);
       for (let i = 0; i < images.length; i += 1) {
         ffmpeg.FS('unlink', `img00${i}.png`);
