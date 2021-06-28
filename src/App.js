95a8a116abee1b7b30ce99e09bb45aeaf87dfa5b
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-06-28 14:22:53 +0700

    create video with loop 3 times

--- src/App.js
+++ src/App.js
@@ -20,23 +20,21 @@ function App() {
     }
   }, false);
 
-  const doTranscode = async (images) => {
-    if (images.length) {
+  const doTranscode = async (imgs) => {
+    if (imgs.length) {
       try {
         const fileName = params?.filename || 'Paper_Wallet__QRCode'
         await ffmpeg.load();
+        const images = [].concat(... new Array(3).fill(imgs));
         for (let i = 0; i < images.length; i += 1) {
           ffmpeg.FS('writeFile', `img00${i}.png`, await fetchFile(images[i]));
         }
-        const last = images.length - 1;
-        ffmpeg.FS('writeFile', `img00${last + 1}.png`, await fetchFile(images[last]));
-        await ffmpeg.run('-pattern_type', 'glob', '-r', '1/2', '-i', '*.png', '-c:v', 'libx264', '-r', '2', '-pix_fmt', 'yuv420p', `${fileName}.mp4`);
+        await ffmpeg.run('-pattern_type', 'glob', '-r', '1', '-i', '*.png', '-c:v', 'libx264', '-r', '2', '-pix_fmt', 'yuv420p', `${fileName}.mp4`);
   
         const data = ffmpeg.FS('readFile', `${fileName}.mp4`);
         for (let i = 0; i < images.length; i += 1) {
           ffmpeg.FS('unlink', `img00${i}.png`);
         }
-        ffmpeg.FS('unlink', `img00${last + 1}.png`);
         const videoUrl = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
         setVideoSrc(videoUrl);
         currentSource.postMessage({
